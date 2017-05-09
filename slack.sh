50674939752b95972a585a85f8bfab6e7ca09826
Daisuke Miyamoto <miyamoto@brain.imi.i.u-tokyo.ac.jp>
2017-05-09 20:53:54 +0900

    fix icon

--- slack.sh
+++ slack.sh
@@ -4,37 +4,44 @@
 url='CHANGEME'		# example: https://hooks.slack.com/services/QW3R7Y/D34DC0D3/BCADFGabcDEF123
 username='Zabbix'
 
+## Values received by this script:
+# To = $1 (Slack channel or user to send the message to, specified in the Zabbix web interface; "@username" or "#channel")
+# Subject = $2 (usually either PROBLEM or RECOVERY)
+# Message = $3 (whatever message the Zabbix action sends, preferably something like "Zabbix server is unreachable for 5 minutes - Zabbix server (127.0.0.1)")
+
 # Get the Slack channel or user ($1) and Zabbix subject ($2 - hopefully either PROBLEM or RECOVERY)
 channel="$1"
 subject="$2"
+message="$3"
 
 # Change message emoji depending on the subject - smile (RECOVERY), frowning (PROBLEM), or ghost (for everything else)
 if [ "$subject" == 'RECOVERY' ]; then
-    emoji=':large_blue_circle:'
+    emoji=':black_circle:'
     color='good'
 elif [ "$subject" == 'PROBLEM' ]; then
-    emoji=':red_circle:'
+    emoji=':black_circle:'
     color='danger'
 else
     emoji=':ghost:'
     color='warning'
 fi
 
-# The message that we want to send to Slack is the "subject" value ($2 / $subject - that we got earlier)
-#  followed by the message that Zabbix actually sent us ($3)
-message="${subject}: $3"
-
 # Build our JSON payload and send it as a POST request to the Slack incoming web-hook URL
 payload="payload={
   \"channel\": \"${channel}\",
   \"username\": \"${username}\",
   \"icon_emoji\": \"${emoji}\",
-  \"text\": \"${message}\",
-  \"attachements\": [
+  \"attachments\": [
     {
-      \"color\": \"${color}\"
+      \"fallback\": \"${message}\",
+      \"color\": \"${color}\",
+      \"title\": \"${subject}\",
+      \"text\": \"${message}\"
     }
   ]
 }"
-#payload="payload={\"channel\": \"${to//\"/\\\"}\", \"username\": \"${username//\"/\\\"}\", \"text\": \"${message//\"/\\\"}\", \"icon_emoji\": \"${emoji}\"}"
+
+#echo $payload
 curl -m 5 --data-urlencode "${payload}" $url -A 'zabbix-slack-alertscript'
+
+
